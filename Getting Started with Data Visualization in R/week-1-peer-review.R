8aecec7d8d494fe552a84352e4b45f427072781b
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2021-03-05 07:57:13 +0800

    Add files via upload

---
+++ Getting Started with Data Visualization in R/week-1-peer-review.R
@@ -0,0 +1,32 @@
+# Week 1 Practice Code
+
+# Type your name here:
+
+# Complete these coding tasks, then submit your R file for another student to compare against the example output.
+
+# Problem 1 
+
+# Create a data frame that includes two columns, one named "Animals" and the other named "Foods". The first column should be this vector (note the intentional repeated values): Dog, Cat, Fish, Fish, Lizard
+
+
+#The second column should be this vector: Bread, Orange, Chocolate, Carrots, Milk
+
+
+#### Write your code below:
+
+
+# Problem 2
+
+# Using the data frame created in Problem 2, use the table() command to create a frequency table for the column called "Animals".
+
+#### Write your code below:
+
+
+# Problem 3
+
+# Use read.csv() to import the survey data included in this assignment. Using that data, make a histogram of the column called "pid7".
+
+
+#### Write your code below:
+
+
